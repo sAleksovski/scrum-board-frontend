37e8db5acd7f51d3bc6bf69296206218f6b08f8b
Stefan Aleksovski <aleksovski.stefan994@gmail.com>
2016-12-01 16:16:44 +0100

    Updated task details view

--- app/controllers/modal/TaskDetailsModalController.js
+++ app/controllers/modal/TaskDetailsModalController.js
@@ -3,13 +3,34 @@
 
     var app = angular.module('scrum-board-frontend');
 
-    app.controller('TaskDetailsModalController', function ($scope, $mdDialog, TaskService, slug, sprintId, task, originalTask) {
+    app.controller('TaskDetailsModalController', function ($scope, $mdDialog, TaskService, BoardService, slug, sprintId, task, originalTask) {
 
         $scope.dificultyList = ['_0', '_1', '_2', '_3', '_5', '_8', '_13', '_21', '_34', '_55', '_89'];
         $scope.priorityList = ['LOW', 'MEDIUM', 'HIGH', 'URGENT'];
 
         $scope.task = task;
         $scope.comment = '';
+
+        $scope.users = [];
+
+        BoardService.getBoard(slug).then(function (response) {
+            response.data.boardUserRole.forEach(function (user) {
+                $scope.users.push(user.user);
+            });
+        });
+
+        $scope.querySearch = function (query) {
+            return query ? $scope.users.filter(createFilterFor(query)) : $scope.users;
+        };
+
+        function createFilterFor(query) {
+            var lowercaseQuery = angular.lowercase(query);
+            return function filterFn(user) {
+                var u = user.firstName + ' ' + user.lastName + ' ' + user.firstName;
+                u = angular.lowercase(u);
+                return (u.indexOf(lowercaseQuery) > -1);
+            };
+        }
 
         $scope.save = function () {
             TaskService.updateTask(slug, sprintId, $scope.task).then(function (response) {
